740bdc3b6c5dd0fbe3a9ef6612651a328c5f92ac
Nils Bestehorn <104395997+nilbest@users.noreply.github.com>
2023-06-04 01:48:17 +0200

    Added basic Lyb for getting started with HX711

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
+unsigned long ReadCount(void);
+
+uint8_t P1 = 2; //uint8_t = byte
+uint8_t P2 = 3;
+
+byte ADDO = P1^5; //Bitwise exklusive OR = XOR
+byte ADSK = P2^0;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ReadCount();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+unsigned long ReadCount(void){ 
+ unsigned long Count; 
+ unsigned char i; 
+ ADDO=1; 
+ ADSK=0; 
+ Count=0; 
+ while(ADDO); 
+ for (i=0;i<24;i++){ 
+  ADSK=1; 
+  Count=Count<<1; 
+  ADSK=0; 
+  if(ADDO) Count++; 
+ } 
+ ADSK=1; 
+ Count=Count^0x800000; 
+ ADSK=0; 
+ return(Count);
 }
